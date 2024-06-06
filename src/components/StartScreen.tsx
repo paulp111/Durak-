ff4a727d4bff567aba209fd0cf80e8889c032b11
paulp111 <133204904+paulp111@users.noreply.github.com>
2024-06-06 22:21:07 +0200

    changed styles

--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -31,10 +31,16 @@ const StartScreen: React.FC = () => {
         <div className="glow"></div>
         <img src={durakLogo} alt="Durak Logo" className="logo" />
       </div>
-      <a onClick={handleStartGame}><span>Play</span></a>
-      <Link to="/howto"><span>How To</span></Link>
-      <Link to="/settings"><span>Settings</span></Link>
-      <Link to="/"><span>Exit</span></Link>
+      <div className="title">
+        <h1>Дурак</h1>
+        <h2>a Soviet card game</h2>
+      </div>
+      <div className="button-container">
+        <a onClick={handleStartGame}><span>Play</span></a>
+        <Link to="/howto"><span>How To</span></Link>
+        <Link to="/settings"><span>Settings</span></Link>
+        <Link to="/"><span>Exit</span></Link>
+      </div>
     </div>
   );
 };
